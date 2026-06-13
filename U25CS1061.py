8f09d4a6ec65596b730a8dca6e8f284b178b9a78
zayyad-007 <zayyadsaddiq007@gmail.com>
2026-06-14 00:54:38 +0100

    Add files via upload

---
+++ U25CS1061.py
@@ -0,0 +1,7 @@
+def almighty(a, b, c):
+    x = (-b + (b**2 - 4*a*c)**0.5) / (2*a)
+    y = (-b - (b**2 - 4*a*c)**0.5) / (2*a)
+    print(f"The Values of x and y are \nX={x}\nY={y}")
+almighty(1, -5, 6)
+print("Where X is the Positive value of the Almighty Formula \nAnd Y is the Negative value")
+print("By U25CS1061 \nZayyad Sadiq \nComputer Science Department")
